0801343ff1364ec12f35248f42bff6fdc5e2faed
Luiz F. Pinheiro <luizfelipenet@hotmail.com>
2020-10-23 01:21:45 -0300

    Database configuration based on docker-compose file

--- my_store/settings.py
+++ my_store/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'my_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_store',
+        'USER': 'postgres',
+        'PASSWORD': 'my_store_secret',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
