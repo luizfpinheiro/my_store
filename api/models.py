dc51ed080f0be12a667aeaa9ee10759f515537b9
Luiz F. Pinheiro <luizfelipenet@hotmail.com>
2020-10-25 00:22:34 -0300

    Create Product and Order models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    price = models.IntegerField(validators=[MinValueValidator(0)])
+    available = models.BooleanField(default=True)
+
+class Order(models.Model):
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Product)
+    price = models.IntegerField(validators=[MinValueValidator(0)])
+    created_at = models.DateTimeField(auto_now=True)
